12d70a290514d62f7118c84cb421f2d0aa3a7939
Luis Alfonso Talegón Puente <luisetex@gmail.com>
2019-11-29 02:39:55 +0100

    add srp code

---
+++ solid/srp_1.py
@@ -0,0 +1,8 @@
+class CalculatorReader:
+    def read_input(self):
+        """Read string from I/O"""
+
+    def parse_operations(self):
+        """Parse the string, indentify the operations and create and operation dictionary with them"""
+
+    def send_operations_dictionary(self):
---
+++ solid/srp_2.py
@@ -0,0 +1,15 @@
+class AlertSystem:
+    def read_command(self):
+        """This method interfaces the reading of an incoming command"""
+
+class OperatorReader:
+    def load(self):
+        """Read operation from the stresam"""
+
+class OperationIdentifier:
+    def identiy_operation(self):
+        """parse the operation"""
+
+class OperationSender:
+    def send_operation(self):
+        """send the dictionary"""
