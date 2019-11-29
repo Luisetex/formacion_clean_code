0ea070678628a7fbbf33116ed59fb22f7bad04c9
Luis Alfonso Talegón Puente <luisetex@gmail.com>
2019-11-29 02:40:35 +0100

    add interface segregation

---
+++ solid/interfacesegregation_1.py
@@ -0,0 +1,6 @@
+class EventParser:
+    def from_xml(self, xml_file):
+        pass
+
+    def from_json(self, json_file):
+        pass
---
+++ solid/interfacesegregation_2.py
@@ -0,0 +1,7 @@
+class JSONEventParser:
+    def from_xml(self, xml_file):
+        pass
+
+class XMLEventParser:
+    def from_json(self, json_file):
+        pass
